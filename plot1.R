133f97e9b035b21196435b96ae08ef248408260b
Bertrand Frottier <bertrand.frottier@gmail.com>
2015-03-04 00:05:07 +0100

    Plot1 description update

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ source('load_project_data.R')  # Loads data
 
 png('plot1.png') # Default size is OK (480x480px)
 
-# First plot is a simple histogram
+# First plot is a simple histogram of global power activity values
 hist(
     hpc.data$Global_active_power,
     xlab='Global active power (kilowatts)',

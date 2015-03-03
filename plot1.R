ede177f8f02091475a72117daa1680ccef311ed0
Bertrand Frottier <bertrand.frottier@gmail.com>
2015-03-03 23:56:49 +0100

    Code for first plot, and output

---
+++ plot1.R
@@ -0,0 +1,13 @@
+source('load_project_data.R')  # Loads data
+
+png('plot1.png') # Default size is OK (480x480px)
+
+# First plot is a simple histogram
+hist(
+    hpc.data$Global_active_power,
+    xlab='Global active power (kilowatts)',
+    main='Global active power',
+    col='red',  
+)
+
+dev.off()

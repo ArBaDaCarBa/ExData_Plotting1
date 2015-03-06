573c4e1b7816be9f0e34494b7a82e8234ceef40b
Bertrand Frottier <bertrand.frottier@gmail.com>
2015-03-06 23:43:56 +0100

    Typo fix

--- plot1.R
+++ plot1.R
@@ -5,8 +5,8 @@ png('plot1.png') # Default size is OK (480x480px)
 # First plot is a simple histogram of global power activity values
 hist(
     hpc.data$Global_active_power,
-    xlab='Global active power (kilowatts)',
-    main='Global active power',
+    xlab='Global Active Power (kilowatts)',
+    main='Global Active Power',
     col='red',  
 )
 
